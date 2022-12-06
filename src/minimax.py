ed6ab2bc947de7c026ec41e6c3393fe7ae30e474
Frainer Encarnación <fraineralex2001@gmail.com>
2022-12-06 01:44:26 -0400

    [IMP] improvement the minimax and the heuristic evaluation

--- src/minimax.py
+++ src/minimax.py
@@ -13,7 +13,7 @@ class Minimax:
     pawnEvalWhite = [
     [ 0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0],
     [ 5.0,  5.0,  5.0,  5.0,  5.0,  5.0,  5.0,  5.0],
-    [ 1.0,  1.0,  2.0,  3.0,  6.0,  2.0,  1.0,  1.0],
+    [ 1.0,  1.0,  2.0,  3.0,  3.0,  2.0,  1.0,  1.0],
     [ 0.5,  0.5,  1.0,  2.5,  2.5,  1.0,  0.5,  0.5],
     [ 0.0,  0.0,  0.0,  2.0,  2.0,  0.0,  0.0,  0.0],
     [ 0.5, -0.5, -1.0,  0.0,  0.0, -1.0, -0.5,  0.5],
@@ -93,7 +93,7 @@ class Minimax:
             time_after = datetime.datetime.now()
             move = chess.Move.from_uci(str(possible_move))
             board.push(move)
-            value = Minimax.minimax(board, depth-1, float("-inf"), float("inf"), not maximizingPlayer, time_before, limit_time)
+            value = Minimax.minimax(board, depth-1, float("-inf"), float("inf"), not maximizingPlayer, time_before, limit_time, 'black')
             board.pop()
             if( value >= bestMove):
                 bestMove = value
@@ -106,14 +106,16 @@ class Minimax:
 
         return bestMoveFound
 
-    def minimax(board: chess.Board, depth, alpha, beta, maximizingPlayer, time_before: datetime.datetime, limit_time):
+    def minimax(board: chess.Board, depth, alpha, beta, maximizingPlayer, time_before: datetime.datetime, limit_time, turn):
         if(depth == 0):
             # Heuristic evaluation
             node_evaluation = 0
-            node_evaluation += Minimax.evaluationBoard(board) * -1
-            node_evaluation += Minimax.checkmate_status(board, node_evaluation, maximizingPlayer)
-            node_evaluation += Minimax.good_square_moves(board, maximizingPlayer)
-            return node_evaluation
+            node_evaluation += Minimax.check_status(board, node_evaluation, turn)
+            node_evaluation += Minimax.evaluationBoard(board)
+            node_evaluation += Minimax.checkmate_status(board, turn)
+            node_evaluation += Minimax.good_square_moves(board, turn)
+            #print('Node puntuation: ',node_evaluation)
+            return -node_evaluation
 
         possibleMoves = board.legal_moves
 
@@ -124,7 +126,7 @@ class Minimax:
                 time_after = datetime.datetime.now()
                 move = chess.Move.from_uci(str(possibleMove))
                 board.push(move)
-                value = Minimax.minimax(board, depth-1, alpha, beta, False, time_before, limit_time)
+                value = Minimax.minimax(board, depth-1, alpha, beta, False, time_before, limit_time, 'black')
                 bestMove = max(bestMove, value)
                 board.pop()
                 alpha = max(alpha, value);
@@ -147,7 +149,7 @@ class Minimax:
                 time_after = datetime.datetime.now()
                 move = chess.Move.from_uci(str(possibleMove))
                 board.push(move)
-                value = Minimax.minimax(board, depth-1, alpha, beta, True, time_before, limit_time)
+                value = Minimax.minimax(board, depth-1, alpha, beta, True, time_before, limit_time, 'white')
                 board.pop()
                 bestMove = min(bestMove, value)
                 if (beta <= alpha):
@@ -228,45 +230,56 @@ class Minimax:
             return  absoluteValue
 
         if (piece == 'k'):
-            absoluteValue = 900 + Minimax.kingEvalBlack[x][y]
+            absoluteValue = 9000 + Minimax.kingEvalBlack[x][y]
             return  absoluteValue * -1
 
         if (piece == 'K'):
-            absoluteValue = 900 + Minimax.kingEvalWhite[x][y]
-            return  -absoluteValue
+            absoluteValue = 9000 + Minimax.kingEvalWhite[x][y]
+            return  absoluteValue
 
         print(f'unknow pice: {piece} in the interval: [{x}],[{y}]')
         return absoluteValue
 
-    def checkmate_status(board: chess.Board, node_evaluation, currently_player):
-        black_points = 0
-        is_check = board.is_check()
+    def checkmate_status(board: chess.Board, turn):
+        node_evaluation = 0
         is_checkmate = board.is_checkmate()
-        turn = "black" if currently_player == False else "white"
+        #turn = "black" if currently_player == False else "white"
+
+        if turn == "white":
+            if (is_checkmate):
+                node_evaluation += float("inf")
+        else:
+            if (is_checkmate):
+                node_evaluation += float("-inf")
+
+        return node_evaluation
+
+    def check_status(board: chess.Board, node_evaluation, turn):
+        black_evaluation = 0
+        is_check = board.is_check()
+        #turn = "black" if currently_player == False else "white"
 
         if turn == "white":
             if (is_check):
-                black_points += 1 * node_evaluation
-            elif (is_checkmate):
-                black_points += float("inf")
+                #print('check status white: True')
+                black_evaluation += 10 #* node_evaluation
         else:
-            if is_check == 1:
-                black_points -= 1 * node_evaluation
-            elif (is_checkmate):
-                black_points += float("-inf")
+            if (is_check):
+                #print('check status black: True')
+                black_evaluation -= 10 #* node_evaluation
 
-        return black_points
+        return black_evaluation
     
-    def good_square_moves(board: chess.Board, currently_player):
+    def good_square_moves(board: chess.Board, turn):
         node_evaluation = 0
-        turn = "black" if currently_player == False else "white"
+        #turn = "black" if currently_player == False else "white"
         square_values = {"e4": 1, "e5": 1, "d4": 1, "d5": 1, "c6": 0.5, "d6": 0.5, "e6": 0.5, "f6": 0.5,
                         "c3": 0.5, "d3": 0.5, "e3": 0.5, "f3": 0.5, "c4": 0.5, "c5": 0.5, "f4": 0.5, "f5": 0.5}
 
         possible_moves = board.legal_moves
         for possible_move in possible_moves:
             move = str(possible_move)
-            if turn == "black":
+            if turn == "white":
                 if move[2:4] in square_values:
                     node_evaluation += square_values[move[2:4]]
             else:
@@ -275,7 +288,7 @@ class Minimax:
                     
         return node_evaluation
 
-    def main(move, next_player, board: chess.Board, limit_time = 3):
+    def main(move, next_player, board: chess.Board, limit_time = 3,):
 
         #board = chess.Board()
 
@@ -295,15 +308,19 @@ class Minimax:
                 return(board)
 
         elif(next_player == 'black'):
-            is_game_over = board.outcome()
-            if(is_game_over):
-                print('Game over')
+            #is_game_over = board.outcome()
+            if(board == None or board == 'Game over'):
+                return print('Game over')
             else:
                 print('----------------')
                 print("Minimax Turn:")
                 print('\n-----------------\nMinimax is Calculating...\n-----------------')
                 time_before = datetime.datetime.now()
                 move = Minimax.minimaxRoot(board, 3, True, time_before, limit_time)
+
+                if(move == None):
+                    return print('Game over')
+
                 time_after = datetime.datetime.now()
                 answerTime = time_after - time_before
                 print(f'Minimax time: {answerTime.seconds} segundos')
